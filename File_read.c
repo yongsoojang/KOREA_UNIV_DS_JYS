14d4a78cf7952d8c08a9dd7adb91fb1ccccc4a3f
YongSoo Jang <YongSoo Jang>
2016-06-03 20:09:36 +0900

    user 파일 read ( name 과 id )

--- File_read.c
+++ File_read.c
@@ -1,14 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+struct User{
+	char id[10];
+	char name[20];
+	struct User* next;
+};
+
+struct User* alloc_user(){
+	struct User* user = (struct User*)malloc(sizeof(struct User));
+	user->next = NULL;
+	return user;
+}
+
+struct User* insert_user(struct User* list, char id[], char name[] ){
+
+	struct User* user = alloc_user();
+	if(list==NULL)
+	{
+		sscanf(id, "%s", user->id);
+		sscanf(name,"%s",user->name);
+		list = user;
+		
+	}
+	else
+	{
+		sscanf(id, "%s", user->id);
+		sscanf(name,"%s",user->name);
+		user->next = list;
+	}
+	
+	return user;
+}
+
+
+void print_user(struct User* list) 
+{	
+	struct User* p = list;
+	int i;
+	
+	for(  ; p != NULL ; p=p->next)
+	{
+		printf("%s\n",p->id);
+		printf("%s\n",p->name);
+	}
+
+
+}
 
 void main()
 {
 	FILE *fa;
-	char tstr[100];
-	fa = fopen("word.txt","r");
+
+	char tstr[50];
+	char name[50];
+	char id[50];
+
+	fa = fopen("user.txt","r");
 	
-	while( fgets(tstr,100,fa) != NULL )
+	int num=0;
+	
+	struct User* user_list = NULL;
+
+	while( fgets(tstr,50,fa) != NULL )
 	{
-		printf("%s",tstr);
+		
+		if(num%4 == 0)
+		{
+			
+			sscanf(tstr,"%s",id);
+			
+		}
+		else if(num%4 == 2)
+		{
+			
+			sscanf(tstr,"%s",name);
+			
+			user_list = insert_user(user_list, id, name);
+		}
+		
+		num = num + 1;
+			
+		
 	}
 	fclose(fa);
+	
+	print_user(user_list);
+	
 }
